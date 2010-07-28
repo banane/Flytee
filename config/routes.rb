cfba1e3079fd8fe998e681b8558a9b6ed63ee32a
Anna Billstrom <banane@gmail.com>
2010-07-28 14:17:10 -0700

    new survey wordage and routing to new flights search on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -39,8 +39,8 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-#   map.root :controller => "flights", :action=>"new"
-	map.root :controller => "users", :action => "index"
+   map.root :controller => "flights", :action=>"new"
+#	map.root :controller => "users", :action => "index"
 
   # See how all your routes lay out with "rake routes"
 
